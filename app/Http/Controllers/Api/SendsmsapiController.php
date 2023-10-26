a125e6de5f684c0e9cf1637f3bac88e5d07fba62
luccas <sh4rck3@gmail.com>
2023-10-26 08:03:28 -0300

    ajustando controle de enviod e mensagem

--- app/Http/Controllers/Api/SendsmsapiController.php
+++ app/Http/Controllers/Api/SendsmsapiController.php
@@ -4,16 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\SendsmsRequest;
+use Illuminate\Http\Resources\Json\JsonResourceCollection;
+use Illuminate\Suporte\Facedes\Log;
+
+
 
 
 class SendsmsapiController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * @retunr AnonymousResourceResource
      */
     public function index()
     {
-        //
+        Log::info("function index");
     }
 
     /**
@@ -21,7 +27,7 @@ class SendsmsapiController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Log::info("function store ");
     }
 
     /**
@@ -29,7 +35,7 @@ class SendsmsapiController extends Controller
      */
     public function show(string $id)
     {
-        //
+        Log::info("function show");
     }
 
     /**
@@ -37,7 +43,7 @@ class SendsmsapiController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        Log::info("fucntion update");
     }
 
     /**
@@ -45,6 +51,6 @@ class SendsmsapiController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        Log::info("function destroy");
     }
 }
